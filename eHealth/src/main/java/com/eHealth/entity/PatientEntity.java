1ba3bb2dc1ed60a400a8a15e403e456174789147
danielabarba <123421203+danielabarba@users.noreply.github.com>
2023-03-15 16:05:38 -0700

    Add files via upload

---
+++ eHealth/src/main/java/com/eHealth/entity/PatientEntity.java
@@ -0,0 +1,26 @@
+package com.eHealth.entity;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+public class PatientEntity {
+    @Id
+    private int id;
+    private LocalDate birthdate;
+
+    public PatientEntity(int id, LocalDate birthdate){
+        this.id = id;
+        this.birthdate = birthdate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+
+}
